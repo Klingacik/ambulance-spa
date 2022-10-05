8ed02d42942e6e3fc833d6dee1dffc380cb85f07
Martin Popper <martin.popper@siemens-healtineers.com>
2022-10-05 07:41:30 +0200

    Adjust app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ambulance-spa';
+  title = 'ambulance-spa-application';
 }
